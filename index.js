36c33594e6a9856b09b17de0787e0b9d1f7af2cd
Asma Kolaib <as.kolaib@gmail.com>
2020-08-18 07:42:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 function map(sourceArray,fun){
 return sourceArray.map(fun);
 }
-
-
 function reduce(sourceArray,fun, startingPoint=0){
   if(startingPoint > 0){
    return sourceArray.reduce(fun,startingPoint);}
